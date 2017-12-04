7909ebc341ce175c798c1caee61c5d1aa3266b45
rrizun <rrizun@gmail.com>
2017-12-03 16:06:34 -0800

    trymat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
+
+interface Wallet {
+  asset: string;
+  weight: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  desired_weights: Map<string, number> = new Map();
+
+  constructor() {
+    this.desired_weights.set("BTC", 50);
+    this.desired_weights.set("ETH", 20);
+    this.desired_weights.set("LTC", 20);
+    this.desired_weights.set("USD", 10);
+
+    // this.desired_weights.keys();
+
+
+  }
+
+  assets() {
+    return Array.from(this.desired_weights.keys());
+  }
+
+  desired_weight(asset: string) {
+    return this.desired_weights.get(asset);
+  }
+
+  inc(s: string) {
+    if (this.desired_weights.get(s)<100)
+      this.desired_weights.set(s, this.desired_weights.get(s) + 1);
+  }
+  dec(s: string) {
+    if (this.desired_weights.get(s)>0)
+      this.desired_weights.set(s, this.desired_weights.get(s) - 1);
+  }
+
+  onChange(asset: string, change: MatSliderChange) {
+    this.desired_weights.set(asset, change.value);
+
+    // for (let i = 0; i < 100; ++i) {
+    //   if (this.desired_weights_sum()!=100) {
+    //     // let over = this.desired_weights_sum() - 100;
+    //     // let under = 100 - this.desired_weights_sum();
+    //     // if (over > 0)
+    //     {
+    //       for (let s of Array.from(this.desired_weights.keys())) {
+    //         if (s != asset) {
+    //           if (this.desired_weights_sum()>100) {
+    //             if (this.desired_weights.get(s)>0)
+    //               this.desired_weights.set(s, this.desired_weights.get(s) - 1);
+    //           }
+    //         }
+    //       }
+    //     }
+    //     // if (under > 0)
+    //     {
+    //       for (let s of Array.from(this.desired_weights.keys())) {
+    //         if (s != asset) {
+    //           if (this.desired_weights_sum()<100) {
+    //             if (this.desired_weights.get(s)<100)
+    //               this.desired_weights.set(s, this.desired_weights.get(s) + 1);
+    //           }                
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+
+  }
+
+  is_valid(): boolean {
+    return this.desired_weights_sum()==100;
+  }
+
+  apply_disabled() {
+    return this.desired_weights_sum()!=100;
+  }
+
+  desired_weights_sum(): number {
+    let result : number = 0;
+    for (let weight of Array.from(this.desired_weights.values()))
+      result += weight;
+    return result;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatSliderModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
